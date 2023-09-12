954144d444f4b4fcf873f97ce6fc2b15def01aa4
Giovanni Fossati <giovannifossati11@gmail.com>
2023-09-12 09:30:34 -0300

    feat(header): added header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
 import { Button } from "./components/ui/button";
+import { Separator } from "./components/ui/separator";
+import { Github } from "lucide-react";
 
 export function App() {
-  return <Button>Teste</Button>
+  return (
+    <div className="min-h-screen flex flex-col">
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">upload.ai</h1>
+
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">
+            Desenvolvido com 💜 no NLW da Rocketseat
+          </span>
+
+          <Separator orientation="vertical" className="h-6" />
+
+          <Button variant="outline">
+            <Github className="w-4 h-4 mr-2" /> Github
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 }
